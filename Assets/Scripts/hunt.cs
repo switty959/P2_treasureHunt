de4d926e228f74b4cc2d2b51ca0e62113392b770
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2019-04-23 10:02:38 +0200

    hunt display
    hunts can be displayed

---
+++ Assets/Scripts/hunt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+   
+    public class hunt
+    {
+        string name;
+        int id;
+        int score;
+        double time;
+        float distance;
+
+        public hunt()
+        {
+            name = "Did someone order a knuckle sandwich?";
+            id = 0;
+            score = 0;
+            time = 0;
+            distance = 0;
+
+        }
+        public hunt(string tempName)
+        {
+            name = tempName;
+            id = 0;
+            score = 0;
+            time = 0;
+            distance = 0;
+        }
+
+        public string getHuntName()
+        {
+        return this.name;
+        }
+        public void getHuntName(GameObject textHolder)
+        {
+            textHolder.GetComponent<Text>().text = this.name;
+        }
+        public void getHuntScore(GameObject textHolder)
+        {
+            textHolder.GetComponent<Text>().text = this.score.ToString();
+        }
+        public void getHuntTime(GameObject textHolder)
+        {
+            textHolder.GetComponent<Text>().text = this.time.ToString();
+        }
+        public void getHuntDistance(GameObject textHolder)
+        {
+            textHolder.GetComponent<Text>().text = this.distance.ToString();
+        }
+        public void setHuntName(string newName)
+        {
+            this.name = newName;
+        }
+}
+    
---
+++ Assets/Scripts/testHunt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testHunt : MonoBehaviour
+{
+    public GameObject canvasHolder;
+    public GameObject placeHolder;
+    public GameObject nameholders;
+    string canvasName = "Canvas";
+    string[] huntNames = {"yes i did sir","have you seen my bear, tibbers" };
+    Vector3 margin = new Vector3(0,0,50);
+    
+    hunt[] hunters = new hunt[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < hunters.Length; i++)
+        {
+            hunters[i] = new hunt();
+            hunters[i].setHuntName(huntNames[i]);
+            canvasHolder = GameObject.Find(canvasName);
+            GameObject newPlaceHolder = Instantiate(placeHolder, canvasHolder.transform );
+            nameholders = placeHolder.transform.GetChild(0).gameObject;
+            hunters[i].getHuntName(nameholders);
+        }
+        
+        
+    }
+
+   
+}
