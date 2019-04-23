f0a5c7a8534aeaefad3452b86f9077e796afb123
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2019-04-23 10:52:51 +0200

    new shit
    added scene to everyones folders

--- Assets/Scripts/testHunt.cs
+++ Assets/Scripts/testHunt.cs
@@ -1,32 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testHunt : MonoBehaviour
 {
     public GameObject canvasHolder;
     public GameObject placeHolder;
     public GameObject nameholders;
-    string canvasName = "Canvas";
-    string[] huntNames = {"yes i did sir","have you seen my bear, tibbers" };
-    Vector3 margin = new Vector3(0,0,50);
+    public string canvasName = "Canvas";
+    public string[] huntNames = {"yes i did sir","have you seen my bear, tibbers","biggus dickus"," i go by many name, but you can call me tim","Lighting bolt!" };
+    public int numbersOfHunts = 5;
+    int margin = 50;
     
-    hunt[] hunters = new hunt[2];
+    public hunt[] hunters = new hunt[5];
+    
     // Start is called before the first frame update
     void Start()
+    {
+        canvasHolder = GameObject.Find(canvasName);
+        Debug.Log(numbersOfHunts.ToString());
+        generateHunts();
+        
+    }
+
+   public void generateHunts()
     {
         for (int i = 0; i < hunters.Length; i++)
         {
             hunters[i] = new hunt();
             hunters[i].setHuntName(huntNames[i]);
-            canvasHolder = GameObject.Find(canvasName);
-            GameObject newPlaceHolder = Instantiate(placeHolder, canvasHolder.transform );
+            GameObject newPlaceHolder = Instantiate(placeHolder, canvasHolder.transform);
+            Vector3 newPosition = newPlaceHolder.transform.position;
+            newPosition.y = newPosition.y - (margin * i);
+            newPlaceHolder.transform.position = newPosition; 
             nameholders = placeHolder.transform.GetChild(0).gameObject;
             hunters[i].getHuntName(nameholders);
         }
-        
-        
     }
-
-   
 }
