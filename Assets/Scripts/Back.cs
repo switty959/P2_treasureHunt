20c06a132c68648eb319c81089a665c73435a2fb
Scott Naylor <scottjamesnaylor@gmail.com>
2019-04-23 14:24:20 +0200

    Back Button Script
    Just a little back button script :)

--- Assets/Scripts/Back.cs
+++ Assets/Scripts/Back.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Back : MonoBehaviour
+
 {
+
+    public GameObject BackButton;
+    public GameObject[] Panels;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        int count = gameObject.transform.GetChildCount()-1;
+        Panels = new GameObject[count];
+        for (int i = 0; i < count; i++)
+        {
+            Panels[i] = gameObject.transform.GetChild(i).gameObject;
+        }
         
     }
 
