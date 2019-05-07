da2f6c5e968d4550ef57f8cd87a7f0f35ebe1201
ScottJamesNaylor <42110788+ScottJamesNaylor@users.noreply.github.com>
2019-05-07 13:50:18 +0200

    Models almost finished
    Just need to tweak the spyglass and painting in Blender as they are acting weird for some reason.

--- Assets/Scripts/clueAnimation.cs
+++ Assets/Scripts/clueAnimation.cs
@@ -7,24 +7,18 @@ public class clueAnimation : MonoBehaviour
 {
 
     public GameObject model;
-    public GameObject scroll;
-
+    
     public AnimationClip Closed;
     public AnimationClip Opening;
     public AnimationClip Open;
-
-    public AnimationClip ScrollClosed;
-    public AnimationClip ScrollOpening;
-    public AnimationClip ScrollOpen;
-
-    Animation scrollAnim;
-    Animation modelAnim;
+   
+    Animator modelAnim;
 
 
     private void Start()
     {
-        scrollAnim = scroll.GetComponent<Animation>();
-        modelAnim = model.GetComponent<Animation>();
+        
+        modelAnim = model.GetComponent<Animator>();
      
     }
 
@@ -33,13 +27,10 @@ public class clueAnimation : MonoBehaviour
 
 
         if (Input.GetKeyDown("1"))
-        {
-
-           
-           scrollAnim.Play(Opening.name.ToString());
-            modelAnim.Play(ScrollOpening.name.ToString());
+        {          
+          
+            modelAnim.Play(Opening.name.ToString());
             
-
         }
 
     }
---
+++ Assets/Scripts/scrollAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class scrollAnimation : MonoBehaviour
+{
+
+   
+    public GameObject scroll;
+
+    public AnimationClip ScrollClosed;
+    public AnimationClip ScrollOpening;
+    public AnimationClip ScrollOpen;
+
+    Animation scrollAnim;
+    
+
+
+    private void Start()
+    {
+        scrollAnim = scroll.GetComponent<Animation>();
+        
+
+    }
+
+    private void Update()
+    {
+
+
+        if (Input.GetKeyDown("1"))
+        {
+
+            scrollAnim.Play(ScrollOpening.name.ToString());
+
+        }
+
+    }
+}
